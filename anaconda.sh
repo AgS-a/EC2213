9fd19714c4ac55f99c87ca40990ceb24aabb4a29
AgS-a <122721657+AgS-a@users.noreply.github.com>
2024-05-22 11:54:33 +0530

    Update anaconda.sh
    added sudo su

--- anaconda.sh
+++ anaconda.sh
@@ -2,10 +2,12 @@
 
 #download anaconda3-2022.10 with python 3.13 in debian based systems
 
-sudo apt update
-sudo apt upgrade
+sudo su
 
-sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+apt update
+apt upgrade
+
+apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
 curl -O https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
 
